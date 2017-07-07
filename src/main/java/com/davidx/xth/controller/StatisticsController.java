ed4532b058d73cd6c03e2d17257568242b810ac7
xc <75736409@qq.com>
2017-07-07 17:31:20 +0800

    添加统计功能

---
+++ src/main/java/com/davidx/xth/controller/StatisticsController.java
@@ -0,0 +1,10 @@
+package com.davidx.xth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xc on 17-7-4.
+ */
+@RestController
+public class StatisticsController {
+}
